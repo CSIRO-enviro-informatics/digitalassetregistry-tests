30a12ded736d027750a3ba062f30ea667564074b
Jevy Wang <jevy.wangfei@gmail.com>
2018-08-23 16:50:08 +1000

    comments

--- load_test.py
+++ load_test.py
@@ -12,7 +12,6 @@ class UserBehavior(TaskSet):
         """ on_start is called when a Locust start before any task is scheduled """
         self.client.verify = False
         locust.events.quitting += self.on_quit
-
         self.fake_user()
 
     def on_quit(self):
@@ -35,6 +34,7 @@ class UserBehavior(TaskSet):
         response = self.client.post("/api/3/action/user_create", headers=headers, data=payload)
         self.api_key = json.loads(response._content)["result"]["apikey"]
 
+    # The number controls the frequence running this the method
     @task(20)
     def login(self):
         self.client.post("/ldap_login_handler", {"login": self.person_id, "password": self.person_password})
@@ -56,20 +56,17 @@ class UserBehavior(TaskSet):
             "author": self.author,
             "verified": True
         })
-        print(self.person_id, self.person_password, self.name, self.client.cookies, self.api_key)
+        # print(self.person_id, self.person_password, self.name, self.client.cookies, self.api_key)
         response = self.client.post("/api/3/action/package_create", headers={'Authorization': self.api_key, 'content-type': 'application/json'}, data=payload)
         self.package_id = json.loads(response.content)['result']['id']
 
-
-
+        # add resources to a dataset/package
         resource_payload = json.dumps({
             "package_id": self.package_id,
             "url": 'http://test/'+self.package_id,
             "name": 'TEST-'+self.package_id
         })
-
         resource_res = self.client.post("/api/3/action/resource_create", headers={'Authorization': self.api_key, 'content-type': 'application/json'}, data=resource_payload)
-        # print(resource_res.__dict__)
 
     @task(20)
     def dataset_query(self):
